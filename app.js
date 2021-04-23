764030894ce486e71f8f120eb42389cd9c151f2d
Gustavo Ennes <gustavo_ennes@yahoo.com>
2021-04-23 10:47:52 -0300

    cors lib added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 const portfolioRouter = require('./routes/portfolio');
 const indexRouter = require('./routes/index')
 const rateLimit = require("express-rate-limit");
+const cors = require('cors')
 
 // Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
 // see https://expressjs.com/en/guide/behind-proxies.html
@@ -26,6 +27,7 @@ if(process.env.NODE_ENV !== 'test') {
 //parse application/json and look for raw text
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors)
 
 //  apply limiter anti-DDos to all requests
 app.use(limiter);
