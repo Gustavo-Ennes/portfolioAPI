b35937bba85e9525c101fa7d93e419137f025518
Gustavo Ennes <gustavo_ennes@yahoo.com>
2021-05-07 14:52:55 -0300

    allowing more headers

--- middleware/portfolio/allowCorsSubdomain.js
+++ middleware/portfolio/allowCorsSubdomain.js
@@ -5,9 +5,9 @@ const middle = (req, res, next) => {
   if (whitelist.indexOf(origin) != -1) {
     res.header("Access-Control-Allow-Origin", origin);
   }
-  res.header("Access-Control-Allow-Headers", ["Content-Type","X-Requested-With","X-HTTP-Method-Override","Accept"]);
+  res.header("Access-Control-Allow-Headers", ["Access-Control-Allow-Headers", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers",  "Content-Type","X-Requested-With","X-HTTP-Method-Override","Accept"]);
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,POST,PUT,DELETE,OPTIONS");
   res.header("Cache-Control", "no-store,no-cache,must-revalidate");
   res.header("Vary", "Origin");
   if (req.method === "OPTIONS") {
