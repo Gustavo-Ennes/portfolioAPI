950d9bd50cc11554bbd98406b6a799039f860afe
Gustavo Ennes <gustavo_ennes@yahoo.com>
2021-05-07 15:08:02 -0300

    allowing https://localhost

--- middleware/portfolio/allowCorsSubdomain.js
+++ middleware/portfolio/allowCorsSubdomain.js
@@ -1,6 +1,9 @@
 const whitelist = ['https://ennes.dev', 'https://www.ennes.dev', 'http://localhost:8080', 'https://localhost:8080'];
 
 const middle = (req, res, next) => {
+  console.log(`
+    req.headers == ${req.headers}
+  `)
   var origin = req.headers.origin;
   if (whitelist.indexOf(origin) != -1) {
     res.header("Access-Control-Allow-Origin", origin);
@@ -8,12 +11,7 @@ const middle = (req, res, next) => {
   res.header("Access-Control-Allow-Headers", ["Access-Control-Allow-Headers", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers",  "Content-Type","X-Requested-With","X-HTTP-Method-Override","Accept"]);
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET,HEAD,POST,PUT,DELETE,OPTIONS");
-  res.header("Cache-Control", "no-store,no-cache,must-revalidate");
-  res.header("Vary", "Origin");
-  if (req.method === "OPTIONS") {
-  res.status(200).send("");
-    return;
-  }
+
   next();
 }
 
