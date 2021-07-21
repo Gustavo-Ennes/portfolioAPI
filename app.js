38db320abef11f7c0e5da3ae2d58e37177c242a9
Gustavo Ennes <gustavo_ennes@yahoo.com>
2021-07-21 11:21:13 -0300

    server.js cleaning

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 require('dotenv').config();
 
-const cors = require("cors")
 const express = require('express');
-const app = express().use('*', cors({
-  origin: ['*'],
-  credentials: true,
-  exposedHeaders: ['set-cookie']
-}))
-const morgan = require('morgan');
+const app = express()
 const bodyParser = require('body-parser');
 const portfolioProjectsRouter = require('./routes/portfolio/portfolio');
 const indexRouter = require('./routes/portfolio/index')
@@ -17,7 +11,6 @@ const rateLimit = require("express-rate-limit");
 const authMiddleware = require('./middleware/kratodo/auth')
 const session = require("express-session")
 const MongoStore = require('connect-mongo')
-const cookieParser = require('cookie-parser')
 
 // Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
 // see https://expressjs.com/en/guide/behind-proxies.html
@@ -28,30 +21,23 @@ app.use(
   secret: 'keyboard cat',
   store: MongoStore.create({mongoUrl:process.env.DB_HOST}),
   saveUninitialized: false,
-  resave: false,
+  resave: true,
   })
 )
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // limit each IP to 100 requests per windowMs
-});
-//don't show the log when it is test
-if(process.env.NODE_ENV !== 'test') {
-    //use morgan to log at command line
-    app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
-}
 
 //parse application/json and look for raw text
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 //  apply limiter anti-DDos to all requests
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
 app.use(limiter);
 
-// whitelist subdomains
-// app.use(whiteListSubdomain)
-app.use(cookieParser());
+
 
 app.use('/', indexRouter)
 app.use('/portfolio/', portfolioProjectsRouter)
